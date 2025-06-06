3b9e7c2bf52b8b2a125a98cad18bb67074a91b02
first <parnuphun1598@gmail.com>
2025-06-06 17:38:27 +0700

    generate git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,101 @@
+'use client';
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [grid, setGrid] = useState<any[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const generateGrid = (width: number, height: number) => {
+    const newGrid = [];
+    for (let i = 0; i < height; i++) {
+      const row = [];
+      for (let j = 0; j < width; j++) {
+        row.push({
+          status: 1,
+          distant: Math.floor(Math.random() * 100),
+        });
+      }
+      newGrid.push(row);
+    }
+    setGrid(newGrid);
+    console.log(newGrid);
+  };
+
+  const gridToggle = (rowIndex: number, cellIndex: number) => {
+    console.log(`Toggling cell at row ${rowIndex}, column ${cellIndex}`);
+    setGrid((prevGrid) => {
+      const newGrid = prevGrid.map((row, rIndex) => {
+        if (rIndex === rowIndex) {
+          return row.map((cell: any, cIndex: any) => {
+            if (cIndex === cellIndex) {
+              return {
+                ...cell,
+                status: cell.status === 1 ? 0 : 1,
+              };
+            }
+            return cell;
+          });
+        }
+        return row;
+      });
+      return newGrid;
+    });
+  };
+
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100 text-black p-6 gap-4">
+      <div className="w-5/12 flex flex-row gap-4">
+        <div className="flex flex-row items-center justify-start gap-4">
+          <input
+            value={width}
+            onChange={(e) => {
+              if (e.target.value && !isNaN(Number(e.target.value))) {
+                setWidth(Number(e.target.value))
+              }
+            }}
+            className="px-4 py-2 border border-gray-300 bg-white rounded focus:outline-none focus:ring-2 focus:ring-blue-400 w-full max-w-md"
+          />
+          <input
+            value={height}
+            onChange={(e) => {
+              if (e.target.value && !isNaN(Number(e.target.value))) {
+                setHeight(Number(e.target.value))
+              }
+            }}
+            className="px-4 py-2 border border-gray-300 bg-white rounded focus:outline-none focus:ring-2 focus:ring-blue-400 w-full max-w-md"
+          />
+          <button
+            onClick={() => generateGrid(width, height)}
+            className="w-24 p-2 bg-blue-500 text-white rounded"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Gen
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div>
+        <div className="grid grid-cols-1 gap-4">
+          {(grid.length > 0) && grid.map((row, rowIndex) => (
+            <div key={rowIndex} className="flex flex-row gap-2">
+              {row.map((cell: any, cellIndex: any) => (
+                <div
+                  onClick={() => {
+                    gridToggle(rowIndex, cellIndex);
+                  }}
+                  key={cellIndex}
+                  className={`w-12 h-12 rounded-lg flex items-center justify-center border ${cell.status === 1 ? 'bg-white' : 'bg-black text-white'}
+                    hover:bg-gray-200 transition-colors cursor-pointer
+                  `}
+                >
+                  {cell.distant}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
